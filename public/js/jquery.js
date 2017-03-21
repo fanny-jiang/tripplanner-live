85010d9719c070a2be3d40c514125b8ab57e8049
Fanny Jiang <jiangf@bu.edu>
2017-03-21 16:16:45 -0400

    got started on jquery

---
+++ public/js/jquery.js
@@ -0,0 +1,8 @@
+$(function initializePlanner() {
+        // $("select[data-type='hotels']").each(function(index) {
+        // `<option>${hotels[index]}</option>`
+    // })
+    console.log(hotels);
+
+
+})
