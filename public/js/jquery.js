ed33f998486ebd57b43e1ca7dae55aec3980b795
ssundby <stefanie.sundby@gmail.com>
2017-03-22 11:00:22 -0400

    refactored code in jquery.js

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,26 +1,31 @@
 $(function initializePlanner() {
 
+    var modelsArray = [hotels, restaurants, activities];
+    var modelNamesArray = ['hotels', 'restaurants', 'activities'];
+
     function createOptionTags () {
-        for(i = 0; i < hotels.length; i++) {
-            $("select[data-type='hotels']").append(`<option>${hotels[i].name}</option>`)
-        }
-
-        for(i = 0; i < restaurants.length; i++) {
-            $("select[data-type='restaurants']").append(`<option>${restaurants[i].name}</option>`)
-        }
-
-        for(i = 0; i < activities.length; i++) {
-            $("select[data-type='activities']").append(`<option>${activities[i].name}</option>`)
+        for(i = 0; i < modelsArray.length; i++) {
+            for(j = 0; j < modelsArray[i].length; j++) {
+                $(`select[data-type=${modelNamesArray[i]}]`).append(`<option>${modelsArray[i][j].name}</option>`);
+            }
         }
     }
 
     function blueAddButtons () {
-        // var selected = '';
 
-        // $("select[data-type='hotels'] option:selected").change(function() {
-        //     selected = $("option:selected")
-        //  })
-
+        // for(i = 0; i < modelNamesArray.length; i++) {
+        //     console.log($(`button[data-type=${modelNamesArray[i]}][data-action='add']`))
+        //     $(`button[data-type=${modelNamesArray[i]}][data-action='add']`).click(function() {
+        //         var data = $(`select[data-type=${modelNamesArray[i]}] option:selected`).val()
+        //         console.log($(`select[data-type=${modelNamesArray[i]}] option:selected`).val());
+        //         // create itinerary item div
+        //         $(`ul.list-group.${modelNamesArray[i]}`).append(
+        //             `<div class="itinerary-item">
+        //                 <span class="title">${data}</span>
+        //                     <button class="btn btn-xs btn-danger remove btn-circle">x</button>
+        //             </div>`)
+        //     });
+        // }
         $("button[data-type='hotels'][data-action='add']").click(function() {
             var data = $("select[data-type='hotels'] option:selected").val()
             // create itinerary item div
